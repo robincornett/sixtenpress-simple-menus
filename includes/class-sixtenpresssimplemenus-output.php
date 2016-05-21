f130a765da70d5101a919fae79dcfbf0ac4df2ee
Robin Cornett <hello@robincornett.com>
2016-05-21 16:06:03 -0400

    Fix term meta fallback

--- includes/class-sixtenpresssimplemenus-output.php
+++ includes/class-sixtenpresssimplemenus-output.php
@@ -5,12 +5,32 @@
  */
 class SixTenPressSimpleMenusOutput {
 
-	protected $menu_key = '_sixtenpress_simplemenu';
-
-	/*
-	 * Once we hit wp_head, the WordPress query has been run, so we can determine if this request uses a custom subnav
+	/**
+	 * Post/term meta key.
+	 * @var string
 	 */
-	public function get_menu( $menu = '' ) {
+	protected $meta_key = '_sixtenpress_simplemenu';
+
+	/**
+	 * Replace the menu selected in the WordPress Menu settings with the custom one for this request
+	 */
+	public function replace_menu( $mods ) {
+
+		$menu = $this->get_menu();
+		if ( $menu ) {
+			$mods['secondary'] = (int) $menu;
+		}
+
+		return $mods;
+	}
+
+	/**
+	 * Return the custom menu for output to the filter.
+	 * @param string $menu
+	 *
+	 * @return mixed|string
+	 */
+	protected function get_menu( $menu = false ) {
 
 		$setting = sixtenpresssimplemenus_get_setting();
 
@@ -25,28 +45,40 @@ class SixTenPressSimpleMenusOutput {
 		if ( is_category() || is_tag() || is_tax() ) {
 			$term = is_tax() ? get_term_by( 'slug', get_query_var( 'term' ), get_query_var( 'taxonomy' ) ) : get_queried_object();
 			if ( $term ) {
-				$term_menu = sixtenpresssimplemenus_get_term_meta( $term, $this->menu_key );
+				$term_menu = sixtenpresssimplemenus_get_term_meta( $term, $this->meta_key );
 				$menu      = $term_menu ? $term_menu : $menu;
 			}
 		}
 		if ( is_singular() ) {
-			$post_menu = sixtenpresssimplemenus_get_menu();
-			$menu      = $post_menu ? $post_menu : $menu;
+			$post_menu = false;
+			if ( ! $menu ) {
+				$post_menu = sixtenpresssimplemenus_get_menu() ? sixtenpresssimplemenus_get_menu() : $this->get_term_menu( $menu );
+			}
+			$menu = $post_menu ? $post_menu : $menu;
 		}
 
 		return $menu;
 	}
 
 	/**
-	 * Replace the menu selected in the WordPress Menu settings with the custom one for this request
+	 * Get the menu related to the most popular taxonomy tied to the post.
+	 * @param $menu
+	 *
+	 * @return int
 	 */
-	public function replace_menu( $mods ) {
+	function get_term_menu( $menu ) {
 
-		$menu = $this->get_menu();
-		if ( $menu ) {
-			$mods['secondary'] = (int) $menu;
+		$taxonomies = get_taxonomies();
+		$args       = array( 'orderby' => 'count', 'order' => 'DESC' );
+		$terms      = wp_get_object_terms( get_the_ID(), $taxonomies, $args );
+
+		foreach ( $terms as $term ) {
+			$menu = sixtenpresssimplemenus_get_term_meta( $term, $this->meta_key );
+			if ( $menu ) {
+				break;
+			}
 		}
 
-		return $mods;
+		return (int) $menu;
 	}
 }
