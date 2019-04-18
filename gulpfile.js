2b443118729bba004f61f48678bb418a1b1cf327
Robin Cornett <hello@robincornett.com>
2019-04-18 09:20:00 -0400

    Update gulp/packages

--- gulpfile.js
+++ gulpfile.js
@@ -1,41 +1,10 @@
+/**
+ * gulpfile.js
+ */
 'use strict';
 
-var projectName  = 'sixtenpress-simple-menus',
-	version      = '1.2.4',
-	destination  = '../../build',
-	gulp         = require( 'gulp' ),
-	zip          = require( 'gulp-zip' ),
-	config       = {
-		sassPath: 'sass',
-		bowerDir: 'bower_components',
-		sixten  : '../sixtenpress'
-	},
-	buildInclude = [
-		'**',
+var gulp       = require( 'gulp' );
+var requireDir = require( 'require-dir' );
 
-		// exclude:
-		'!node_modules/**/*',
-		'!bower_components/**/*',
-		'!sass/**/*',
-		'!dist/**/*',
-		'!node_modules',
-		'!bower_components',
-		'!sass',
-		'!dist',
-		'!gulpfile.js',
-		'!package.json',
-		'!bower.json'
-	];
-
-gulp.task( 'assets', function () {
-	gulp.src( config.sixten + '/includes/common/**' )
-		.pipe( gulp.dest( 'includes/common' ) );
-} );
-
-gulp.task( 'zip', function () {
-	return gulp.src( buildInclude, { base: '../' } )
-		.pipe( zip( projectName + '.' + version + '.zip' ) )
-		.pipe( gulp.dest( destination ) );
-} );
-
-gulp.task( 'build', [ 'zip' ] );
+// Require all tasks in gulp/tasks, including subfolders
+requireDir( './gulp/tasks', { recurse: true } );
